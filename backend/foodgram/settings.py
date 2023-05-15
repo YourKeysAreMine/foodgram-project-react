6f69843c27a8e6a493f98c03c011feece0c3abe5
YourKeysAreMine <morfoperdanul@gmail.com>
2023-05-15 22:08:51 +0300

    stable v_1.35 deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'recipes.pagination.PageLimitPagination',
     'PAGE_SIZE': 5,
 }
 
